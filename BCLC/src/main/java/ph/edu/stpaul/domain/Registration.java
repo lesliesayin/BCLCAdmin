a8e9d15c4e32adc430d18896c8187d306dc9f3f1
LeslieAnneSayin <sayin.leslieanne@gmail.com>
2016-02-15 15:18:23 +0800

    Overriding a hashmap and equals method on Registration class

--- BCLC/src/main/java/ph/edu/stpaul/domain/Registration.java
+++ BCLC/src/main/java/ph/edu/stpaul/domain/Registration.java
@@ -127,6 +127,7 @@ public class Registration {
 		return address;
 	}
 
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
@@ -288,5 +289,31 @@ public class Registration {
 				+ ", motherLandlineNumber=" + motherLandlineNumber + ", motherMobileNumber=" + motherMobileNumber
 				+ ", level=" + level + ", cluster=" + cluster + ", adviser=" + adviser + ", schedule=" + schedule
 				+ ", startTime=" + startTime + ", endTime=" + endTime + "]";
+	}
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((studentId == null) ? 0 : studentId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Registration other = (Registration) obj;
+		if (studentId == null) {
+			if (other.studentId != null)
+				return false;
+		} else if (!studentId.equals(other.studentId))
+			return false;
+		return true;
 	}
 }
